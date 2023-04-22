c56f195be96ebf1f426b6b705a486b2dadbe8b48
jari.valentine <jari.valentine@student.howest.be>
2023-04-22 19:47:17 +0200

    sliding carousel of outfits

--- app/src/main/java/be/howest/jarivalentine/virtualcloset/ui/OutfitScreen.kt
+++ app/src/main/java/be/howest/jarivalentine/virtualcloset/ui/OutfitScreen.kt
@@ -1,30 +1,37 @@
 package be.howest.jarivalentine.virtualcloset.ui
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import be.howest.jarivalentine.virtualcloset.R
 import be.howest.jarivalentine.virtualcloset.data.Outfit
 import be.howest.jarivalentine.virtualcloset.data.labels
 import be.howest.jarivalentine.virtualcloset.data.outfits
-import be.howest.jarivalentine.virtualcloset.data.tags
 import be.howest.jarivalentine.virtualcloset.ui.theme.Shapes
+import be.howest.jarivalentine.virtualcloset.ui.theme.VirtualClosetTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
 
 @Composable
 fun OutfitScreen() {
@@ -65,42 +72,61 @@ fun SearchField() {
     )
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun Outfits() {
-    LazyRow() {
-        items(outfits) {
-            Outfit(it)
+    SlidingCarousel(
+        itemsCount = outfits.size,
+        itemContent = { index ->
+            Outfit(outfits[index])
+        }
+    )
+}
+
+@Composable
+fun Outfit(outfit: Outfit) {
+    Card {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = outfit.imageResourceId),
+                contentDescription = stringResource(
+                    id = outfit.name
+                ),
+                modifier = Modifier
+                    .width(LocalConfiguration.current.screenWidthDp.dp - 20.dp)
+                    .height(LocalConfiguration.current.screenHeightDp.dp - 320.dp),
+                contentScale = ContentScale.Crop
+            )
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .width(LocalConfiguration.current.screenWidthDp.dp - 30.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column() {
+                    Text(text = stringResource(id = outfit.name), fontSize = 20.sp)
+                    Labels()
+                }
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "More",
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
         }
     }
 }
 
 @Composable
-fun Outfit(outfit: Outfit) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(id = outfit.imageResourceId),
-            contentDescription = stringResource(
-                id = outfit.name
-            ),
-            modifier = Modifier
-                .width(LocalConfiguration.current.screenWidthDp.dp - 20.dp)
-                .height(LocalConfiguration.current.screenHeightDp.dp - 300.dp),
-            contentScale = ContentScale.Crop
-        )
-        Row(
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Column() {
-                Text(text = stringResource(id = outfit.name), fontSize = 20.sp)
-                LazyRow() {
-                    items(labels) {
-                        Label(name = it)
-                    }
-                }
-            }
-        }
+fun Labels() {
+    Row() {
+        Label(labels[2])
+        Label("Available")
     }
 }
 
@@ -120,5 +146,89 @@ fun Label(name: String) {
             modifier = Modifier.padding(5.dp),
             text = name
         )
+    }
+}
+
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun SlidingCarousel(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState = remember { PagerState() },
+    itemsCount: Int,
+    itemContent: @Composable (index: Int) -> Unit,
+) {
+    val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
+
+    Box(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        HorizontalPager(count = itemsCount, state = pagerState) { page ->
+            itemContent(page)
+        }
+        Surface(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.BottomCenter),
+            shape = CircleShape,
+            color = Color.Black.copy(alpha = 0.5f)
+        ) {
+            DotsIndicator(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
+                totalDots = itemsCount,
+                selectedIndex = if (isDragged) pagerState.currentPage else pagerState.targetPage,
+                dotSize = 8.dp
+            )
+        }
+    }
+}
+
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color = Color.White,
+    unSelectedColor: Color = Color.Gray,
+    dotSize: Dp
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        items(totalDots) { index ->
+            IndicatorDot(
+                color = if (index == selectedIndex) selectedColor else unSelectedColor,
+                size = dotSize
+            )
+
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun IndicatorDot(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    color: Color
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(color)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OutfitPreview() {
+    VirtualClosetTheme {
+        OutfitScreen()
     }
 }
